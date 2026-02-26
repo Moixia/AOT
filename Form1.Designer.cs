af704134d81e12a50440211b33084e2e1be8febf
Moixia <moisesnbplay@gmail.com>
2026-02-26 15:44:11 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.MainFolderDirTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ActualFileDirTextBox = new System.Windows.Forms.TextBox();
+            this.FileSystemTreeView = new System.Windows.Forms.TreeView();
+            this.RichTextBoxContent = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.FileSystemTreeView);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.MainFolderDirTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.RichTextBoxContent);
+            this.splitContainer1.Panel2.Controls.Add(this.ActualFileDirTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // MainFolderDirTextBox
+            // 
+            this.MainFolderDirTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainFolderDirTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainFolderDirTextBox.Location = new System.Drawing.Point(0, 0);
+            this.MainFolderDirTextBox.Name = "MainFolderDirTextBox";
+            this.MainFolderDirTextBox.Size = new System.Drawing.Size(266, 24);
+            this.MainFolderDirTextBox.TabIndex = 0;
+            this.MainFolderDirTextBox.Text = "C:\\Users\\modt\\Desktop\\WindowsSSH\\Docs";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(0, 430);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "/help";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox3.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(0, 430);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(530, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "/help";
+            // 
+            // ActualFileDirTextBox
+            // 
+            this.ActualFileDirTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ActualFileDirTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActualFileDirTextBox.Location = new System.Drawing.Point(0, 0);
+            this.ActualFileDirTextBox.Name = "ActualFileDirTextBox";
+            this.ActualFileDirTextBox.Size = new System.Drawing.Size(530, 24);
+            this.ActualFileDirTextBox.TabIndex = 3;
+            // 
+            // FileSystemTreeView
+            // 
+            this.FileSystemTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileSystemTreeView.Location = new System.Drawing.Point(0, 24);
+            this.FileSystemTreeView.Name = "FileSystemTreeView";
+            this.FileSystemTreeView.Size = new System.Drawing.Size(266, 406);
+            this.FileSystemTreeView.TabIndex = 2;
+            // 
+            // RichTextBoxContent
+            // 
+            this.RichTextBoxContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RichTextBoxContent.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RichTextBoxContent.Location = new System.Drawing.Point(0, 24);
+            this.RichTextBoxContent.Name = "RichTextBoxContent";
+            this.RichTextBoxContent.Size = new System.Drawing.Size(530, 406);
+            this.RichTextBoxContent.TabIndex = 4;
+            this.RichTextBoxContent.Text = resources.GetString("RichTextBoxContent.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView FileSystemTreeView;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox MainFolderDirTextBox;
+        private System.Windows.Forms.RichTextBox RichTextBoxContent;
+        private System.Windows.Forms.TextBox ActualFileDirTextBox;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AOT
+{
+    public partial class Form1 : Form
+    {
+        public string StartDir { get; set; } = @"C:\Users\modt\Desktop\Playground";
+
+        [StructLayout(LayoutKind.Sequential)] struct POINT { public int X, Y; }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        struct CF2
+        {
+            public int cb; public uint mk, ef; public int h, o, tc; public byte cs, pf;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] public string fn;
+            public short w, s; public int bc, lcid, ck; public short k; public byte ut, an, ra;
+        }
+
+        [DllImport("user32.dll")] static extern IntPtr SendMessage(IntPtr h, int m, IntPtr w, ref CF2 l);
+        [DllImport("user32.dll")] static extern IntPtr SendMessage(IntPtr h, int m, IntPtr w, IntPtr l);
+        [DllImport("user32.dll")] static extern IntPtr SendMessage(IntPtr h, int m, IntPtr w, ref POINT l);
+
+        private bool _upd = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RichTextBoxContent.DetectUrls = false;
+            SendMessage(RichTextBoxContent.Handle, 0x0400 + 77, (IntPtr)0x02, (IntPtr)0);
+
+            Load += (s, e) => MainFolderDirTextBox.Text = StartDir;
+
+            MainFolderDirTextBox.TextChanged += (s, e) => {
+                FileSystemTreeView.Nodes.Clear();
+                if (Directory.Exists(MainFolderDirTextBox.Text))
+                {
+                    FileSystemTreeView.Nodes.Add(FillNode(new DirectoryInfo(MainFolderDirTextBox.Text)));
+                    FileSystemTreeView.ExpandAll();
+                }
+            };
+
+            FileSystemTreeView.AfterSelect += (s, e) => {
+                string path = e.Node.Tag?.ToString();
+                if (File.Exists(path))
+                {
+                    ActualFileDirTextBox.Text = path;
+                    RichTextBoxContent.Text = File.ReadAllText(path);
+                }
+                else if (Directory.Exists(path ?? e.Node.FullPath))
+                {
+                    ActualFileDirTextBox.Text = "";
+                    var files = Directory.GetFiles(path ?? e.Node.FullPath, "*.aot", SearchOption.TopDirectoryOnly)
+                                         .Select(f => $"[{Path.GetFileName(f)}] @{f}");
+                    RichTextBoxContent.Text = string.Join(Environment.NewLine, files);
+                }
+                RefreshFormatting();
+            };
+
+            RichTextBoxContent.TextChanged += (s, e) => { if (!_upd) RefreshFormatting(); };
+        }
+
+        private void RefreshFormatting()
+        {
+            if (string.IsNullOrEmpty(RichTextBoxContent.Text)) return;
+
+            _upd = true;
+            POINT scroll = new POINT();
+            SendMessage(RichTextBoxContent.Handle, 0x0400 + 221, IntPtr.Zero, ref scroll);
+            int start = RichTextBoxContent.SelectionStart, len = RichTextBoxContent.SelectionLength;
+
+            SendMessage(RichTextBoxContent.Handle, 0x000B, IntPtr.Zero, IntPtr.Zero);
+
+            try
+            {
+                // 1. RESET TOTAL
+                RichTextBoxContent.SelectAll();
+                SetLink(false);
+                RichTextBoxContent.SelectionColor = RichTextBoxContent.ForeColor;
+                RichTextBoxContent.SelectionFont = new Font(RichTextBoxContent.Font, FontStyle.Regular);
+
+                // 2. APLICAR CURSIVAS Y NEGRITAS PRIMERO (con precaución)
+                // Solo aplica si el asterisco/guion bajo está rodeado de espacios o al inicio/fin de línea
+                // Esto evita que "aot_file" se convierta en cursiva.
+                ApplyStyle(@"(?<=^|\s)\*(.*?)\*(?=$|\s)", FontStyle.Bold);
+                ApplyStyle(@"(?<=^|\s)_(.*?)_(?=$|\s)", FontStyle.Italic);
+
+                // 3. SOBREESCRIBIR CON ENLACES Y ETIQUETAS (Prioridad alta)
+                // Al hacerlo después, los enlaces limpian cualquier cursiva accidental
+
+                // Enlaces (@ y http)
+                foreach (Match m in Regex.Matches(RichTextBoxContent.Text, @"(@[^\n\r]+|https?://\S+)"))
+                {
+                    RichTextBoxContent.Select(m.Index, m.Length);
+                    SetLink(true);
+                    RichTextBoxContent.SelectionColor = Color.CornflowerBlue;
+                    // Forzamos que los links no tengan cursiva/negrita heredada
+                    RichTextBoxContent.SelectionFont = new Font(RichTextBoxContent.Font, FontStyle.Regular);
+                }
+
+                // Etiquetas [Texto]
+                foreach (Match m in Regex.Matches(RichTextBoxContent.Text, @"\[(.*?)\]"))
+                {
+                    RichTextBoxContent.Select(m.Index, m.Length);
+                    SetLink(false); // Los corchetes no son links
+                    RichTextBoxContent.SelectionColor = Color.MediumSeaGreen;
+                    RichTextBoxContent.SelectionFont = new Font(RichTextBoxContent.Font, FontStyle.Bold);
+                }
+
+                RichTextBoxContent.Select(start, len);
+                SendMessage(RichTextBoxContent.Handle, 0x0400 + 222, IntPtr.Zero, ref scroll);
+            }
+            finally
+            {
+                SendMessage(RichTextBoxContent.Handle, 0x000B, (IntPtr)1, IntPtr.Zero);
+                RichTextBoxContent.Invalidate();
+                _upd = false;
+            }
+        }
+
+        private void ApplyStyle(string pattern, FontStyle style)
+        {
+            foreach (Match m in Regex.Matches(RichTextBoxContent.Text, pattern))
+            {
+                RichTextBoxContent.Select(m.Index, m.Length);
+                RichTextBoxContent.SelectionFont = new Font(RichTextBoxContent.Font, style);
+            }
+        }
+
+        private void SetLink(bool add)
+        {
+            var cf = new CF2 { cb = Marshal.SizeOf(typeof(CF2)), mk = 0x20, ef = add ? 0x20u : 0 };
+            SendMessage(RichTextBoxContent.Handle, 0x0400 + 68, (IntPtr)1, ref cf);
+        }
+
+        private TreeNode FillNode(DirectoryInfo d) =>
+            new TreeNode(d.Name, d.GetDirectories().Select(FillNode)
+                .Concat(d.GetFiles().Where(f => f.Extension.ToLower() == ".aot")
+                .Select(f => new TreeNode(f.Name) { Tag = f.FullName }))
+                .ToArray())
+            { Tag = d.FullName };
+    }
+}
